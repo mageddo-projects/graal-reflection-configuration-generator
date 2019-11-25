cd5a6feef532e58e9dd0110890097a9ed25a0804
Elvis de Freitas <edigitalb@gmail.com>
2019-11-25 00:52:17 -0300

    creating more tests

--- acceptance-tests/src/test/java/com/acme/ReflectionConfigGenerationTest.java
+++ acceptance-tests/src/test/java/com/acme/ReflectionConfigGenerationTest.java
@@ -2,6 +2,7 @@ package com.acme;
 
 import org.junit.jupiter.api.Test;
 
+import static com.acme.TestUtils.getResourceAsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReflectionConfigGenerationTest {
@@ -11,11 +12,20 @@ class ReflectionConfigGenerationTest {
 		// arrange
 
 		// act
-		final String reflectJson = TestUtils.getResourceAsString("/META-INF/native-image/com.acme/reflect.json");
+		final String reflectJson = getResourceAsString("/META-INF/native-image/com.acme/reflect.json");
 
 		// assert
-		assertEquals(TestUtils.getResourceAsString("/reflection-config-generation-test/001.json"), reflectJson);
+		assertEquals(getResourceAsString("/reflection-config-generation-test/001.json"), reflectJson);
+	}
+
+	@Test
+	void mustConfigureNativeImagePropertiesFile(){
+		// arrange
+
+		// act
+		final String propsContent = getResourceAsString("/META-INF/native-image/com.acme/native-image.properties");
 
 		// assert
+		assertEquals(getResourceAsString("/reflection-config-generation-test/002.properties"), propsContent);
 	}
 }
