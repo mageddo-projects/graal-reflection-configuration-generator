45e9d27466cd5843709af0a75d033f50f16ec187
Elvis de Freitas <edigitalb@gmail.com>
2019-11-25 01:35:06 -0300

    separating projects

--- example/build-native-image.sh
+++ example/build-native-image.sh
@@ -1,3 +1,7 @@
+set -e
+
 SCRIPT_DIR=$(dirname $0)
-./gradlew example:fatJar
-native-image -J-Xmx5G --no-server -cp "${SCRIPT_DIR}/build/libs/example-all.jar"
+cd $SCRIPT_DIR
+
+./gradlew fatJar
+native-image -J-Xmx5G --no-server -cp ./build/libs/example-all.jar
