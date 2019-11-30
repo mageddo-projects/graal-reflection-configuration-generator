ac5ac935f4c2ca82add108587386de858d42172f
Elvis de Freitas <edigitalb@gmail.com>
2019-11-30 10:27:36 -0300

    setup new gpg keys

--- builder.sh
+++ builder.sh
@@ -20,18 +20,19 @@ case $1 in
 
 		# preparing gpg key
 		mkdir -p $HOME/.gnupg/
-		openssl aes-256-cbc -d -k ${ENCRYPTION_KEY} -in files/deployment/.gnupg/secring.gpg -out $HOME/.gnupg/secring.gpg
+		openssl aes-256-cbc -d -pbkdf2 -k ${GPG_CIPHER_KEY} -in files/deployment/.gnupg/secring.gpg.encrypted -out $HOME/.gnupg/secring.gpg
 
 		# building and deploying to sonatype nexys
-		export GRADLE_PROJECT_OPTS="-PossrhUsername=mageddo -PossrhPassword=${SIGNING_PASSWORD}"
-		export GRADLE_PROJECT_OPTS="${GRADLE_PROJECT_OPTS} -Psigning.password=${SIGNING_PASSWORD} -Psigning.keyId=${SIGNING_KEY_ID}"
+		export GRADLE_PROJECT_OPTS="-PossrhUsername=${NEXUS_USERNAME} -PossrhPassword=${NEXUS_PASSWORD}"
+		export GRADLE_PROJECT_OPTS="${GRADLE_PROJECT_OPTS} -Psigning.password=${NEXUS_PASSWORD} -Psigning.keyId=${GPG_KEY_ID}"
 		export GRADLE_PROJECT_OPTS="${GRADLE_PROJECT_OPTS} -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg"
+		export GRADLE_PROJECT_OPTS="${GRADLE_PROJECT_OPTS} -PrepositoryUrl=$REPOSITORY_URL"
 
 		./gradlew build publishToNexus closeAndReleaseRepository ${GRADLE_PROJECT_OPTS}
 
 		# publishing tag
-		GITHUB_REPO_URL=$(cat gradle.properties | grep 'githubRepoUrl' | awk -F = '{ print $2}')
-		REMOTE="https://${REPO_TOKEN}@github.com/mageddo/graal-reflection-configuration-generator.git"
+		GITHUB_REPO_URL=$(cat gradle.properties | grep 'repositoryUrl' | awk -F = '{ print $2}')
+		REMOTE="https://${REPO_TOKEN}@$(echo $GITHUB_REPO_URL | awk -F '//' '{print $2}')"
 		APP_VERSION=$(./gradlew -q version)
 		git tag ${APP_VERSION}
 		git push "$REMOTE" --tags
