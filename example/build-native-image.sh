b1705b601e40066339034a7158281639153bb8f4
Elvis de Freitas <edigitalb@gmail.com>
2019-11-30 14:55:59 -0300

    saetup mvn example

--- example/build-native-image.sh
+++ example/build-native-image.sh
@@ -3,5 +3,11 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR
 
+echo "> compiling"
 ./gradlew fatJar
+
+echo "> generating native image"
 native-image -J-Xmx5G --no-server -cp ./build/libs/example-all.jar
+
+echo "> running"
+./reflection-configura
