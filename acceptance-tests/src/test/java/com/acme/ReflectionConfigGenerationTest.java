b9bad2ac7e810ed798449d48973d9afc72ab66aa
Elvis de Freitas <edigitalb@gmail.com>
2019-11-30 21:48:13 -0300

    fixing test randomnes

--- acceptance-tests/src/test/java/com/acme/ReflectionConfigGenerationTest.java
+++ acceptance-tests/src/test/java/com/acme/ReflectionConfigGenerationTest.java
@@ -1,6 +1,15 @@
 package com.acme;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import static com.acme.TestUtils.getResourceAsString;
 import static com.acme.TestUtils.sortJson;
@@ -8,17 +17,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReflectionConfigGenerationTest {
 
+	private final ObjectMapper objectMapper = new ObjectMapper()
+		.enable(SerializationFeature.INDENT_OUTPUT)
+		;
+
 	@Test
-	void mustConfigureReflectJson(){
+	void mustConfigureReflectJson() throws JsonProcessingException {
 		// arrange
 
 		// act
-		final String reflectJson = getResourceAsString("/META-INF/native-image/com.acme/reflect.json");
+		final List<JsonNode> items = readReflectConfig();
 
 		// assert
-		assertEquals(
-			sortJson(getResourceAsString("/reflection-config-generation-test/001.json")), sortJson(reflectJson)
-		);
+		assertEquals(10, items.size());
+		assertEquals(getResourceAsString("/reflection-config-generation-test/001.json"), objectMapper.writeValueAsString(items));
+	}
+
+	private List<JsonNode> readReflectConfig() throws JsonProcessingException {
+		List<JsonNode> items = new ArrayList<>();
+		final JsonNode reflectItems = objectMapper.readTree(getResourceAsString("/META-INF/native-image/com.acme/reflect.json"));
+		for (JsonNode jsonNode : reflectItems) {
+			items.add(jsonNode);
+		}
+		Collections.sort(items, new Comparator<JsonNode>() {
+			@Override
+			public int compare(JsonNode a, JsonNode b) {
+				return a.at("/name").asText().compareTo(b.at("/name").asText());
+			}
+		});
+		return items;
 	}
 
 	@Test
